bbdccd91174357ba08dd9e8002217751730ee7e3
Danul De Leon <danul.deleon@gmail.com>
2018-08-14 13:14:54 -0700

    bar chart is more appropriate

--- js/app.js
+++ js/app.js
@@ -138,27 +138,34 @@ function displayResults() {
     showResults.innerHTML = `${Product.allProducts[i].votes} votes for the ${Product.allProducts[i].name}. Rating: ${rating}.`
     results.appendChild(showResults)
   }
-  //attempting to add a donut chart
+  //attempting to add a bar chart
   var ctx = document.getElementById('donut').getContext('2d');
   var myDoughnutChart = new Chart(ctx, {
-    type: 'doughnut',
+    type: 'bar',
     data: {
-    datasets: [{
-        data: [10, 20, 30],
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [{
+        label: '# of Votes',
+        data: [10, 20, 30, 21, 44, 10],
         backgroundColor: [
-          'rgba(255, 99, 132, 0.9)',
-          'rgba(54, 162, 235, 0.9)',
-          'rgba(255, 206, 86, 0.9)']
-    }],
-
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
-        'Red',
-        'Blue',
-        'Yellow'
-    ]
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+      }],    
     },
-    // options: options
+    options: {
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero:true
+              }
+          }]
+      }
+    }
   });
 }
 
